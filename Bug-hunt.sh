2e7b117761c99d7e1ead0d6a0604bd8928599a57
Mathew Adam Groves <grovesmat49@gmail.com>
2025-04-14 14:00:10 +0800

    Create Bug-hunt.sh
    Bug-hunt2
    
    Signed-off-by: Mathew Adam Groves <grovesmat49@gmail.com>

---
+++ Bug-hunt.sh
@@ -0,0 +1,186 @@
+
+# Title: Bug-Hunt
+# Description: Script to install various bug hunting tools on mobile phone/termux
+
+# Exit immediately if a command exits with a non-zero status
+# set -e
+# set arcive at [primary:root/downloads]
+ 
+# Update and upgrade Termux packages
+# pkg update -y && pkg upgrade -y
+
+# Install necessary tools
+# pkg install -y git curl wget python python2 ruby rust golang erlang stylus nethunter metasploit-ng
+
+# GitHub base URL
+# GITHUB_BASE_URL="https://github.com/user"
+
+# Function to git clone and install tool
+# install_tool($1) {
+  #  local tool_Bug-hunt =$1
+   # git clone "${GITHUB_BASE_URL}/${tool_Bug-hunt.sh}"
+    # cd"${tool_Bug-hunt.sh}"
+   # chmod +x install.sh
+   # ./install.sh
+   # cd ..Bug-hunt.sh
+}
+  # cwget git vagrant packer python3-pip && cwget superuser profile setup && install at dir_path:[primary:root/user/data/proc/data/su/usr/dir/superuser:]
+
+# Function build apk && package-bulider 'make' ,&& install necessary packages:with superuser-profile at dir_path:[primary:root/user/data/proc/data/su/usr/dir/superuser:]
+
+# Build a apk exectable for Bug-hunt mkdir path [primary:root/user/data/proc/data/su/usr/dir/superuser:]
+
+# Download and start Bug-hunt using system downloader & arcive at [primary:root/user/data/proc/data/su/usr/dir/superuser:]
+#  start cwget https://github.com/mathew-sudo/Bug-hunt.git
+
+# Build the Bug-hunt environment with Package-Builder
+# TMPDIR=/var/tmp ./build.sh package build --only=virtualmachine-iso ./package/templates/Android v7.apk
+{ …¬
+#  add TheFatRat clone setup 
+   #Setup TheFatRat
+#  git clone https://github.com/Screetsec/TheFatRat.git
+#  chmod +x setup.sh
+  # sudo bash ./setup.sh
+  #  chmod +x chk_tools 
+   #  sudo bash ./chk_tools
+  #  add kraken setup 
+    #  Setup Kraken
+ #  git clone https://github.com/jasonxtn/kraken.git
+ # pip3 install -r requirements.txt "root@localhost"
+    #   chmod +x chk_tools
+  #   python kraken.py
+  # Install bruteforce tools
+ # sudo apt install -y hydra ncrack-ng john }
+…¬
+## Build package (tools requirements needed to run)
+ #./system dev xml-packages/build-package.sh ../packages/device-xml-to-build=Bug-hunt
+ #./start-builder.dev ./build-package.sh name-of-package=Bug-hunt
+………
+#### make && Build and Release
+#bin-debug/
+#bin-release/
+#[Bb]in/
+#[Oo]bj/
+
+###### include Other files and folders
+#.settings/java-code-js+arry_object
+#c/c++object+arry_object
+#exsploit-triggers+ctrl+cli
+#navigation-ctrl+cli
+#user-interface+ctrl+cli
+#image-interface+ctrl+cli
+#build_system-engine+ctrl+cli+executables
+
+#.executables triggers
+# *.hunt-exec [command] [command] 
+# *.hunt-all
+# *.hunt-apk
+# *.hunt-filemapper-ng
+# *.hunt-endpoint_tracer-ng
+# *.hunt-nettapper-ng
+# *.hunt-loghunter-ng
+# *.hunt-locationmapper-ng
+# *.hunt-aircrack-ng
+# *.hunt-Bruteforce
+# *.hunt-ncrack-ng
+# *.hunt-kali-nethunter
+# *.hunt-metasploit
+# *.hunt-hydra
+# *.hunt-dns_decoder
+# *.hunt-proctracker
+# *.hunt-wps_decoder
+# *.hunt-wep_decoder
+# *.hunt-wep2_decoder
+# *.hunt-wep3_decoder
+# *.hunt-xml
+# *.hunt-tunnalprotacol
+ 
+ # /u file_migration-setup dir_path [primary:root/user/data/proc/data/su/usr/dir/superuser:]
+…
+# Project files, i.e. .project, .actionScriptProperties and .flexProperties
+# should NOT be excluded as they contain compiler settings and other important
+# information for Eclipse / Flash Builder.
+ 
+# Install tools
+# install_tool "hunt-exec" ["command"] ["command"]
+# install_ "hunt-all"
+# install_ "hunt-apk"
+# install_tool "filemapper-ng"
+# install_tool "endpoint_tracer-ng"
+# install_tool "nettapper-ng"
+# install_tool "loghunter-ng"
+# install_tool "kali nethunter"
+# install_tool "metasploit"
+# install_tool "hydra"
+# install_tool "dns_decoder"
+# install_tool "proctracker"
+# install_tool "hunt-wps_decoder"
+# install_tool "wep_decoder"
+# install_tool "wep2_decoder"
+# install_tool "wep3_decoder"
+# install_tool "xml"
+# install_tool "tunnalprotacol"
+# install_tool "locationmapper"
+# install_tool "endpoint_tracer-ng"
+# install_tool "aircracker-ng"
+# Install Metasploit
+# pkg install -y
+# ncrack-ng
+# Install Bruteforce tools
+# pkg install -y hydra ncrack-ng
+…¬
+# Uncomment the following lines to install ImageMagick and process images
+# pkg install -y imagemagick
+# convert input_image.png -resize 800x600 output_image.png
+
+#setup all-exsploits && tools for Bug hunt at username: [Bughunter]   passwd: [Bughunt]
+# run c/c++ return config-display-overlay background color#00ff234fff title name:Bug hunt { color#000fff557
+  #  'border: solid border color: Blue font: opendyslexic font color:dark green background color:Black.
+  #  Setting: 'tool luancher slection screen' { settind: 'exsploit luancher slection screen' setting: 'clock' setting: 'file management detaled list view' tool: 'logger'
+#### [Display]
+# overlay_background_color = #00ff234fff
+# title_name = Bug-hunt
+# title_color = #000fff557
+# border_style = solid
+# border_color = Blue
+# font = opendyslexic
+# font_color = dark green
+# background_color = Black
+
+#### [Tools]
+#tool_launcher_selection_screen = true
+#exploit_launcher_selection_screen = true
+#clock_display = true
+#file_management_detailed_list_view = true
+#logger_enabled = true
+#include <iostream>
+#include <string>
+
+ #setupDisplay() {
+   # std::string overlay_background_color = "#00ff234fff";
+   # std::string title_name = "Bug hunt";
+   # std::string title_color = "#000fff557";
+   # std::string border_style = "solid";
+   # std::string border_color = "Blue";
+   # std::string font = "opendyslexic";
+   # std::string font_color = "dark green";
+   # std::string background_color = "Black";
+
+  #setting up the display () {
+   # std::cout << "Setting up display with the following properties:" << std::endl;
+   # std::cout << "Overlay Background Color: " << overlay_background_color << std::endl;
+   # std::cout << "Title: " << title_name << std::endl;
+   # std::cout << "Title Color: " << title_color << std::endl;
+   # std::cout << "Border Style: " << border_style << std::endl;
+   # std::cout << "Border Color: " << border_color << std::endl;
+   # std::cout << "Font: " << font << std::endl;
+   # std::cout << "Font Color: " << font_color << std::endl;
+   # std::cout << "Background Color: " << background_color << std::endl;
+}
+
+# int main() {
+    #setupDisplay=(-y);
+    #return 0;
+}
+
+# Run start.sh script # bash start.sh # or bash ./start.sh
